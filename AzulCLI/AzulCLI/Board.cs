e4e6cd2cab4b16b3e464fb3b8a96cc790598522a
juhtyg <sadovsky11143@gmail.com>
2024-08-23 21:01:23 +0200

    Write initialization to predefined wall and storage

--- AzulCLI/AzulCLI/Board.cs
+++ AzulCLI/AzulCLI/Board.cs
@@ -1,15 +1,14 @@
-using System.Numerics;
 
 namespace AzulCLI;
 
 public class Board
 {
-    public Player[] players { get; private set; }
-    public Plate[] plates { get; private set; }
-    public CenterPlate center { get; private set; }
+    public Player[] Players { get; private set; }
+    public Plate[] Plates { get; private set; }
+    public CenterPlate Center { get; private set; }
     public Tiles storage { get; private set; }
-    public int currentPlayer { get; private set; }
-    public int phase { get; private set; }
+    public int CurrentPlayer { get; private set; }
+    public int Phase { get; private set; }
     public AzulCLI.Vector2 calculating; //on x is currently calculated player and on the y is the row we are on
     public int[,] predefinedWall { get; private set; }
     private bool isAdvanced;
@@ -17,24 +16,46 @@ public class Board
 
     public Board(int playerCount, string[] playerNames) {
         //TODO: check if length of playerNames is same as playerCount
-        plates = new Plate[playerCount * 2 + 1];
-        players = new Player[playerCount];
-        for (int i = 0; i < playerCount; i++) {
-            players[i] = new Player(playerNames[i]);
+        Center = new CenterPlate();
+        storage = new Tiles(Globals.TYPE_COUNT, Globals.TOTAL_TILE_COUNT); 
+        
+        Plates = new Plate[playerCount * 2 + 1];
+        for (int i = 0; i < playerCount * 2 + 1; i++) {
+            Plates[i] = new Plate();
         }
+        
+        Players = new Player[playerCount];
+        for (int i = 0; i < playerCount; i++) {
+            Players[i] = new Player(playerNames[i]);
+        }
+
+        predefinedWall = new int[Globals.TYPE_COUNT, Globals.TYPE_COUNT];
+        for (int i = 0; i < Globals.TYPE_COUNT; i++) {
+            for (int j = 0; j < Globals.TYPE_COUNT; j++) {
+                predefinedWall[j % Globals.TYPE_COUNT, (i + j) % Globals.TYPE_COUNT] = i;
+            }
+        }
+
+        CurrentPlayer = 0;
     }
     
     public bool Move(int plateId, int tileId, int bufferId) {   //center is always last
-        if (plateId > plates.Length) return false;
+        if (plateId > Plates.Length) return false;
         Plate p;
-        if (plateId == plates.Length) {
-            p = center;
+        bool isFirstInCenter = false;
+        if (plateId == Plates.Length) {
+            p = Center;
+            if (!fisrtTaken) {
+                isFirstInCenter = true;
+                fisrtTaken = true;
+            }
         } else {
-            p = plates[plateId];
+            p = Plates[plateId];
         }
         var data = p.GetCounts();
         if (data[tileId].count == 0) return false;
-        bool success = players[currentPlayer].Place(bufferId, data[tileId]);//TODO: check if its from center for bool
+        
+        bool success = Players[CurrentPlayer].Place(bufferId, data[tileId], isFirstInCenter);
 
         #region succieded
         
@@ -45,40 +66,41 @@ public class Board
             for (int i = 0; i < newData.Length; i++) {
                 toPut.PutTile(newData[i]);
             }
-            center.AddTiles(toPut);
+            Center.AddTiles(toPut);
             if (ArePlatesEmpty()) {
-                currentPlayer = 0;
-                while (!players[currentPlayer].hasFullBuffer()) {
-                    currentPlayer++;
-                    if (currentPlayer == players.Length) {
+                CurrentPlayer = 0;
+                while (!Players[CurrentPlayer].hasFullBuffer()) {
+                    CurrentPlayer++;
+                    if (CurrentPlayer == Players.Length) {
                         //TODO: skip phase 2
                     }
                 }
-                phase = 2;
+                Phase = 2;
             }
-            currentPlayer++;
-            if (currentPlayer == players.Length) currentPlayer = 0;
+            CurrentPlayer++;
+            if (CurrentPlayer == Players.Length) CurrentPlayer = 0;
         }
         #endregion
         
         return success;
     }
     
-    //TODO: Check if player has some full Buffers
     public bool Calculate(int col) {
-        int[] fullBuffers = players[calculating.x].FullBuffers();
+        int[] fullBuffers = Players[calculating.x].FullBuffers();
         
-        bool isFilled = players[calculating.x].Fill(fullBuffers[0], col);
+        bool isFilled = Players[calculating.x].Fill(fullBuffers[0], col);
         if (isFilled && fullBuffers.Length == 1) {
-            players[calculating.x].ClearFloor();
+            bool isFirst = Players[calculating.x].ClearFloor();
+            CurrentPlayer = calculating.x;
             calculating.x++;
-            while (!players[calculating.x].hasFullBuffer()) {
+            while (!Players[calculating.x].hasFullBuffer()) {
                 calculating.x++;
-                if (calculating.x == players.Length) phase = 1;
-            } if (calculating.x == players.Length) {
+                if (calculating.x == Players.Length) Phase = 1;
+            } if (calculating.x == Players.Length) {
                 //All players finished phase 2
-                phase = 1;
-                //TODO: set currentPlayer based on the first from center point
+                Phase = 1;
+                fisrtTaken = false;
+                
             }
         }
         
@@ -88,12 +110,12 @@ public class Board
     
 
     private bool ArePlatesEmpty() {
-        foreach (var plate in plates) {
+        foreach (var plate in Plates) {
             if (!plate.isEmpty) {
                 return false;
             }
         }
 
-        return center.isEmpty;
+        return Center.isEmpty;
     }
 }
