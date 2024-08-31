bd287a6ff563f03b8535f248392f9aa86b25571c
juhtyg <sadovsky11143@gmail.com>
2024-08-31 02:02:20 +0200

    cache update

--- Azul/Azul/obj/Debug/net8.0/Azul.AssemblyInfo.cs
+++ Azul/Azul/obj/Debug/net8.0/Azul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Azul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6279dd23e56c717201e2b167307a413137d39a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Azul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Azul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
