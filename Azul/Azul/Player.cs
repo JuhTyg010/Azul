6279dd23e56c717201e2b167307a413137d39a4b
juhtyg <sadovsky11143@gmail.com>
2024-08-31 02:00:57 +0200

    make library

---
+++ Azul/Azul/Player.cs
@@ -0,0 +1,127 @@
+using System.Diagnostics;
+
+namespace Azul;
+
+public class Player
+{
+    private const int floorSize = 7;
+    public string name { get; private set; }
+    public int pointCount { get; private set; }
+    public int[,] wall { get; private set; }
+    private Buffer[] buffers;
+    public List<int> floor { get; private set; }
+    public bool isFirst { get; private set; }
+
+    public Player(string name) {
+        this.name = name;
+        pointCount = 0;
+        wall = new int[5, 5];
+        floor = new List<int>();
+        List<Buffer> bufferList = new List<Buffer>();
+        for (int i = 0; i < wall.GetLength(0); i++) {
+            
+            bufferList.Add(new Buffer(i + 1));
+            
+            for (int j = 0; j < wall.GetLength(1); j++) {
+                wall[i, j] = Globals.EMPTY_CELL;
+            }
+        }
+
+        buffers = bufferList.ToArray();
+    }
+    
+    public bool Place(int row, Tile tile, bool isFirst = false) {
+        if (!possibleRow(row, tile.id)) return false;
+        if (!possibleBuffer(row, tile.id)) return false;
+        if (isFirst) {
+            this.isFirst = true;
+            floor.Add(Globals.FIRST);
+        }
+        int toFloor = tile.count - buffers[row].Empty();
+        bool answer = buffers[row].Assign(tile);
+        
+        if (answer && floor.Count < floorSize) {
+            for (int i = 0; i < toFloor; i++) {
+                floor.Add(tile.id);
+                if (floor.Count == floorSize) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public Tile GetBufferData(int row) {
+        if (buffers[row].typeId == -1) ; //TODO: return somehow that this is empty
+        Debug.Assert(row < buffers.Length, "You're asking for out of range");
+        return new Tile(buffers[row].typeId, buffers[row].filled);
+    }
+
+    public int[] FullBuffers() {
+        if (!hasFullBuffer()) return [];
+        List<int> listOfFull = new List<int>();
+        for (int i = 0; i < buffers.Length; i++) {
+            if(buffers[i].IsFull()) listOfFull.Add(i);
+        }
+
+        return listOfFull.ToArray();
+    }
+
+    public bool Fill(int row, int col) {
+        if (!buffers[row].IsFull()) return false;
+        //TODO: logs
+        if (!possibleColumn(col, buffers[row].typeId)) return false;
+        //TODO:logs
+        wall[row, col] = buffers[row].typeId;
+        //TODO: assign points, check for win condition
+        buffers[row].Clear();
+        return true;
+    }
+
+    public bool ClearFloor() {
+        //negative points are -1,-1,-2,-2,-2,-3,-3
+        int[] toRemove = { 0, -1, -2, -4, -6, -8, -11, -14 };
+        pointCount += toRemove[Math.Min(7, floor.Count)];
+
+        if (floor[0] == Globals.FIRST) {
+            floor.Clear();
+            return true;
+        }
+        floor.Clear();
+        return false;
+    }
+
+    public int FloorSize() {
+        return floorSize;
+    }
+    
+    private bool possibleRow(int row, int typeId) {
+        for (int i = 0; i < wall.GetLength(1); i++) {
+            if (wall[row, i] == typeId) return false;
+        }
+
+        return true;
+    }
+
+    private bool possibleBuffer(int bufferId, int typeId) {
+        if (buffers[bufferId].typeId == Globals.EMPTY_CELL ||
+            buffers[bufferId].typeId == typeId) return true;
+        return false;
+    }
+
+    private bool possibleColumn(int col, int typeId) {
+        for (int i = 0; i < wall.GetLength(0); i++) {
+            if (wall[i, col] == typeId) return false;
+        }
+
+        return true;
+    }
+
+    public bool hasFullBuffer() {
+        foreach (var buffer in buffers) {
+            if (buffer.IsFull()) return true;
+        }
+        return false;
+    }
+}
---
+++ Azul/Azul/Utils.cs
@@ -0,0 +1,74 @@
+namespace Azul;
+
+public static class Globals {
+    public const int FIRST = 999;
+    public const int TYPE_COUNT = 5;
+    public const int TOTAL_TILE_COUNT = 100;
+    public const int PLATE_VOLUME = 4;
+    public const int EMPTY_CELL = -1;
+}
+
+public enum Phase { Taking = 1, Placing = 2, GameOver = 3 }
+public struct Buffer
+{
+    public int size { get; private set; }
+    public int typeId { get; private set; }
+    public int filled { get; private set; }
+    
+    public Buffer(int size) {
+        this.size = size;
+        typeId = Globals.EMPTY_CELL;
+        filled = Globals.EMPTY_CELL;
+    }
+
+    public bool Assign(int id, int count) {//TODO:: maybe return int which is leak( what was after limit)
+        if (filled != Globals.EMPTY_CELL && typeId != id) return false; //TODO: break, return some kind of exeption
+        if (typeId == Globals.EMPTY_CELL) filled = 0;
+        typeId = id;
+        filled += count;
+        filled = Math.Min(filled, size);
+        return true;
+    }
+
+    public bool Assign(Tile tile) {
+        return Assign(tile.id, tile.count);
+    }
+
+    public int Empty() {
+        if (typeId == Globals.EMPTY_CELL) return size;
+        return size - filled;
+    }
+
+    public void Clear() {
+        typeId = Globals.EMPTY_CELL;
+        filled = Globals.EMPTY_CELL;
+    }
+
+    public bool IsFull() {
+        return size == filled;
+    }
+}
+
+public struct Tile
+{
+    public int id;
+    public int count;
+
+    public Tile(int id, int count)
+    {
+        this.id = id;
+        this.count = count;
+    }
+}
+
+public struct Vector2
+{
+    public int x;
+    public int y;
+
+    public Vector2(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
