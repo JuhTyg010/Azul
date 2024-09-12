38ebce8dc617747fad1b7209469e18a49d47c975
juhtyg <sadovsky11143@gmail.com>
2024-09-12 13:21:18 +0200

    Make more readable code and more secure

--- randomBot/randomBot/Bot.cs
+++ randomBot/randomBot/Bot.cs
@@ -14,62 +14,33 @@ public class Bot {
 
     public string DoMove(Board board) {
         Player me = board.Players[id];
-        List<int> posibleBuffers = new List<int>();
-        for (int i = 0; i < Globals.WALL_DIMENSION; i++) {
-            var data = me.GetBufferData(i);
-            if (data.count < i + 1) {
-                posibleBuffers.Add(i);
-            }
-        }
-        int chosenBuffer = posibleBuffers[random.Next(posibleBuffers.Count)];
-        int chosenType = -1;    //not chosen yet
-        int chosenPlate = -1;   //not chosen yet
-        if (me.GetBufferData(chosenBuffer).id == Globals.EMPTY_CELL) {
-            //choose any plate and non 0 value from it
-            List<int> possiblePlates = new List<int>();
-            for(int i = 0; i < board.Plates.Length; i++) {
-                if (!board.Plates[i].isEmpty) {
-                    possiblePlates.Add(i);
-                }
-            }
-            if(!board.Center.isEmpty) possiblePlates.Add(board.Plates.Length);
+        List<int> possibleBuffers = PossibleBuffers(me);
+        bool isCorrectConfig = false;
+        int chosenBuffer = Globals.EMPTY_CELL;
+        int chosenType = Globals.EMPTY_CELL;    //not chosen yet
+        int chosenPlate = Globals.EMPTY_CELL;   //not chosen yet
+        
+        while (!isCorrectConfig) {
+            if(possibleBuffers.Count == 0) throw new Exception("No possible buffers found aka need to put something on floor");
+            int randomPos = random.Next(0, possibleBuffers.Count);
+            chosenBuffer = possibleBuffers[randomPos];
+            possibleBuffers.RemoveAt(randomPos);
             
-            chosenPlate = possiblePlates[random.Next(possiblePlates.Count)];
-            if (chosenPlate == board.Plates.Length) {
-                var possible = board.Center.GetCounts();
-                List<int>possibleTypes = new List<int>();
-                for (int i = 0; i < possible.Length; i++) {
-                    if (possible[i].count != 0) {
-                        possibleTypes.Add(i);
-                    }
-                }
-                chosenType = possibleTypes[random.Next(possibleTypes.Count)];
-            }
-            else {
-                var possible = board.Plates[chosenPlate].GetCounts();
-                List<int>possibleTypes = new List<int>();
-                for (int i = 0; i < possible.Length; i++) {
-                    if (possible[i].count != 0) {
-                        possibleTypes.Add(i);
-                    }
-                }
-                chosenType = possibleTypes[random.Next(possibleTypes.Count)];
-            }
+            List<int> possibleTypes = PossibleTypes(me, chosenBuffer);
+            while (true) {
+                if (possibleTypes.Count == 0) break;
+                
+                randomPos = random.Next(0, possibleTypes.Count);
+                chosenType = possibleTypes[randomPos];
+                possibleTypes.RemoveAt(randomPos);
 
-        }
-        else {
-            chosenType = me.GetBufferData(chosenBuffer).id;
-            List<int> possiblePlates = new List<int>();
-            for (int i = 0; i < board.Plates.Length; i++) {
-                if (!board.Plates[i].isEmpty && board.Plates[i].TileCountOfType(chosenType) != 0) {
-                    possiblePlates.Add(i);
+                List<int> possiblePlates = PossiblePlates(board, chosenType);
+                if (possiblePlates.Count > 0) {
+                    isCorrectConfig = true;
+                    break;
                 }
             }
-            if(!board.Center.isEmpty && board.Center.TileCountOfType(chosenType) != 0) 
-                possiblePlates.Add(board.Plates.Length);
-            chosenPlate = possiblePlates[random.Next(possiblePlates.Count)];
         }
-
         return $"{chosenPlate} {chosenType} {chosenBuffer}";
     }
 
@@ -78,14 +49,14 @@ public class Bot {
         var row = me.FullBuffers()[0];
         List<int> possiblePositions = new List<int>();
         for (int i = 0; i < Globals.WALL_DIMENSION; i++) {
-            if (possibleCol(me.wall, row, i, me.GetBufferData(row).id)) {
+            if (PossibleCol(me.wall, row, i, me.GetBufferData(row).id)) {
                 possiblePositions.Add(i);
             }
         }
         return $"{possiblePositions[random.Next(possiblePositions.Count)]}";
     }
 
-    private bool possibleCol(int[,] wall, int row, int column, int chosenType) {
+    private bool PossibleCol(int[,] wall, int row, int column, int chosenType) {
         for (int i = 0; i < Globals.WALL_DIMENSION; i++) {
             if (wall[i, column] == chosenType) {
                 return false;
@@ -93,4 +64,53 @@ public class Bot {
         }
         return wall[row, column] == Globals.EMPTY_CELL;
     }
+
+    private List<int> PossiblePlates(Board board, int chosenType) {
+        List<int> possiblePlates = new List<int>();
+        
+        for (int i = 0; i < board.Plates.Length; i++) {
+            if (board.Plates[i].GetCounts()[chosenType].count > 0) {
+                possiblePlates.Add(i);
+            }
+        }
+
+        if (board.Center.GetCounts()[chosenType].count > 0) possiblePlates.Add(board.Plates.Length);
+        return possiblePlates;
+    }
+
+    private List<int> PossibleTypes(Player me, int chosenBuffer) {
+        List<int> possibleTypes = new List<int>();
+        if(me.GetBufferData(chosenBuffer).id != Globals.EMPTY_CELL) 
+            return new List<int>(me.GetBufferData(chosenBuffer).id);
+        for (int i = 0; i < Globals.TYPE_COUNT; i++) {
+            bool isPossible = true;
+            for (int j = 0; j < Globals.WALL_DIMENSION; j++) {
+                if (me.wall[chosenBuffer, j] == i) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if (isPossible) {
+                for (int j = 0; j < Globals.WALL_DIMENSION; j++) {
+                    if (PossibleCol(me.wall, chosenBuffer, j, i)) {
+                        possibleTypes.Add(i);
+                    }
+                }
+            }
+        }
+        return possibleTypes;
+    }
+
+    private List<int> PossibleBuffers(Player me) {
+        List<int> posibleBuffers = new List<int>();
+        for (int i = 0; i < Globals.WALL_DIMENSION; i++) {
+            var data = me.GetBufferData(i);
+            if (data.count < i + 1) {
+                posibleBuffers.Add(i);
+            }
+        }
+
+        return posibleBuffers;
+    }
+
 }
