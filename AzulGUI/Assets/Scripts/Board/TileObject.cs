895f73ec80d1f823fd9a9aaff987066b79884be1
juhtyg <sadovsky11143@gmail.com>
2024-09-14 16:05:27 +0200

    Created method to be able correctly initialize the tile with it's plate

--- AzulGUI/Assets/Scripts/Board/TileObject.cs
+++ AzulGUI/Assets/Scripts/Board/TileObject.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 namespace Board {
     
@@ -9,30 +10,34 @@ namespace Board {
     public class TileObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
         [SerializeField] List<Sprite> sprites = new List<Sprite>();
-        public int id; 
+        public int id;
         private Image image;
+        private Plate myPlate;
         
-        void Start()
+        void Awake()
         {
             image = GetComponent<Image>();
         }
-
-        // Update is called once per frame
-        void Update()
-        {
+        
+        public void Initialize(Plate plate, int id_) {
+            myPlate = plate;
+            id = id_;
+            image = GetComponent<Image>();
             image.sprite = sprites[id];
+            
         }
 
         void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData) {
-            
+            GetComponent<RectTransform>().localScale = Vector3.one * 1.5f;
         }
 
         void IPointerExitHandler.OnPointerExit(PointerEventData eventData) {
-            
+            GetComponent<RectTransform>().localScale = Vector3.one;
         }
 
         void IPointerClickHandler.OnPointerClick(PointerEventData eventData) {
-            
+            myPlate.PutInHand(id);
+            //TODO: animation of holding
         }
     }
 }
