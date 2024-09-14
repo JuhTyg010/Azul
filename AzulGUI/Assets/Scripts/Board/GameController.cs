86cde8817c08e3d545cd6bfc2c65daf4e8ac8415
juhtyg <sadovsky11143@gmail.com>
2024-09-14 15:27:09 +0200

    edited data passing between scenes

--- AzulGUI/Assets/Scripts/Board/GameController.cs
+++ AzulGUI/Assets/Scripts/Board/GameController.cs
@@ -1,42 +1,67 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Board {
     public class GameController : MonoBehaviour
-    { 
+    {
         public GameObject tile;
-        [SerializeField] private GameObject playerBoardPrefab = null;
+        
         [SerializeField] private GameObject otherPlayersPanel;
+        [SerializeField] private GameObject platesPanel;
+        
+        [SerializeField] private GameObject playerBoardPrefab;
+        [SerializeField] private GameObject platePrefab;
+        
+        [SerializeField] private Vector2 firstPlatePosition;
+        [SerializeField] private Vector2 plateOffset;
+        [SerializeField] private bool isFlippedY = false;
+        
         
         
         
         private Azul.Board board;
+        public List<GameObject> plates = new List<GameObject>();
         
         
         void Start()
         {
-            using StreamReader reader = new StreamReader(Application.dataPath + "game_config.txt");
-            string line = reader.ReadLine();
-            string[] data = line.Split(' ');
-            
-            int playerCount = int.Parse(data[0]);
+            int playerCount = PlayerPrefs.GetInt("PlayerCount");
+            Debug.Log(playerCount);
             List<int> bots = new List<int>();
             string[] names = new string[playerCount];
+            string[] passedNames = PlayerPrefs.GetString("PlayerNames").Split('\n');
+            string[] passedTypes = PlayerPrefs.GetString("PlayerTypes").Split('\n');
             
-            for (int i = 1; i <= playerCount; i++) {
-                string[] playerData = data[i].Split('_');
-                if(playerData[0] == "B") bots.Add(i);
-                names[i-1] = playerData[1];
+            for (int i = 0; i < playerCount; i++) {
+                Debug.Log(passedNames[i]);
+                Debug.Log(passedTypes[i]);
+                if(passedTypes[i] == "AI") bots.Add(i);
+                names[i] = passedNames[i];
             }
             
             board = new Azul.Board(playerCount, names);
+            Debug.Log(board.Plates.Length);
+            GeneratePlates(board.Plates.Length);
         }
 
         // Update is called once per frame
         void Update()
         {
             
+        }
+
+        private void GeneratePlates(int plateCount) {
+            Vector3 currentPosition = (Vector3) firstPlatePosition;
+            for (int i = 0; i < plateCount; i++) {
+                var plate = Instantiate(platePrefab, currentPosition, Quaternion.identity, platesPanel.transform);
+                plates.Add(plate);
+                plate.GetComponent<RectTransform>().anchoredPosition = currentPosition;
+                currentPosition.x += plateOffset.x;
+                if(isFlippedY) currentPosition.y *= -1;
+                else currentPosition.y += plateOffset.y;
+            }
         }
     }
 }
