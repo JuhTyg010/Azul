511fabf57d7d952a4afda1ef2e7858dc9ed4d6ec
juhtyg <sadovsky11143@gmail.com>
2024-09-14 15:28:06 +0200

    added PutTiles to let gameManager to handle tiles in there

--- AzulGUI/Assets/Scripts/Board/Plate.cs
+++ AzulGUI/Assets/Scripts/Board/Plate.cs
@@ -1,34 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
+
 
 namespace Board {
     public class Plate : MonoBehaviour {
     
         [SerializeField] private Vector2[] localPositions;
-        private Azul.Plate plate;
+        private List<GameObject> tiles = new List<GameObject>();
+        private List<GameObject> inHand = new List<GameObject>();
     
         private GameController gameController;
         void Start()
         {   
             gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
-            plate = new Azul.Plate();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (!plate.isEmpty) {
-                var tiles = plate.GetCounts();
-                int index = 0;
-                foreach (var tile in tiles) {
-                    while (tile.count > 0) {
-                        var currentTile = Instantiate(gameController.tile, transform.position + (Vector3)localPositions[index], Quaternion.identity);
-                        currentTile.GetComponent<TileObject>().id = tile.id;
-                        index++;
-                    }
-                }
-            }
+            
         }
 
+        public void PutTiles(int[] tilesIds) {
+            tiles.Clear();
+            int current = 0;
+            foreach (var id in tilesIds) {
+                var tile = Instantiate(gameController.tile);
+                tile.GetComponent<TileObject>().id = id;
+                tiles.Add(tile);
+                tile.GetComponent<RectTransform>().anchoredPosition = localPositions[current];
+                current++;
+            }
+        }
     }
 }
