9c11d392ebbfd721877cf27e3b071c546b8a9c8f
juhtyg <sadovsky11143@gmail.com>
2024-09-14 15:26:26 +0200

    because of unity using old c# imported this way

---
+++ AzulGUI/Assets/Resources/Board.cs
@@ -0,0 +1,222 @@
+
+using System;
+
+namespace Azul {
+    public class Board
+    {
+        public Player[] Players { get; private set; }
+        public Plate[] Plates { get; private set; }
+        public CenterPlate Center { get; private set; }
+        public Tiles storage { get; private set; }
+        public int CurrentPlayer { get; private set; }
+        public Phase Phase { get; private set; }
+        public int[,] predefinedWall { get; private set; }
+        public bool isAdvanced { get; private set; }
+        public bool fisrtTaken;
+        private bool isGameOver;
+        private int nextFirst;
+
+        public Board(int playerCount, string[] playerNames, bool isAdvanced_ = false) {
+            Logger.WriteLine(" ");
+            Logger.WriteLine("-----------------------------Game start-----------------------------");
+            //TODO: check if length of playerNames is same as playerCount
+            isAdvanced = isAdvanced_;
+            Center = new CenterPlate(Globals.TYPE_COUNT);
+            storage = new Tiles(Globals.TYPE_COUNT, Globals.TOTAL_TILE_COUNT); 
+        
+            Plates = new Plate[playerCount * 2 + 1];
+            for (int i = 0; i < playerCount * 2 + 1; i++) {
+                Plates[i] = new Plate(Globals.TYPE_COUNT);
+            }
+
+            Players = InitializePlayers(playerCount, playerNames);
+
+            predefinedWall = new int[Globals.WALL_DIMENSION, Globals.WALL_DIMENSION];
+            for (int i = 0; i < Globals.WALL_DIMENSION; i++) {
+                for (int j = 0; j < Globals.WALL_DIMENSION; j++) {
+                    predefinedWall[j % Globals.WALL_DIMENSION, (i + j) % Globals.WALL_DIMENSION] = i;
+                }
+            }
+
+            CurrentPlayer = 0;
+            Phase = Phase.Taking;
+            FillPlates();
+        }
+    
+        public bool Move(int plateId, int tileId, int bufferId) {   //center is always last
+            Logger.WriteLine("Move:");
+            Logger.Write("Plate data: ");
+            for (int i = 0; i < Plates.Length; i++) {
+                Logger.Write($" id: {i} {Plates[i]},");
+            }
+            Logger.WriteLine($" id: {Plates.Length} {Center}");
+            Logger.Write($"Player {Players[CurrentPlayer].name} ({CurrentPlayer}) taking from plate {plateId} tile {tileId} to buffer {bufferId}: ");
+            if (Phase != Phase.Taking) {
+                Logger.WriteLine("Invalid Phase");
+                throw new IllegalOptionException("Invalid Phase");
+            }
+
+            if (plateId > Plates.Length) {
+                Logger.WriteLine("invalid plate");
+                return false;
+            }
+            Plate p;
+            bool isFirstInCenter = false;
+            if (plateId == Plates.Length) {
+                p = Center;
+                if (!fisrtTaken) {
+                    isFirstInCenter = true;
+                    fisrtTaken = true;
+                }
+            } else {
+                p = Plates[plateId];
+            }
+            var data = p.GetCounts();
+            if (data[tileId].count == 0) {
+                Logger.WriteLine("no tiles");
+                return false;
+            }
+        
+            bool success = Players[CurrentPlayer].Place(bufferId, data[tileId], isFirstInCenter);
+
+            #region succieded
+
+            if (success) {
+                p.TakeTile(tileId);
+                var newData = p.GetCounts();
+                p.ClearPlate();
+                Tiles toPut = new Tiles(newData.Length, 0);
+                for (int i = 0; i < newData.Length; i++) {
+                    toPut.PutTile(newData[i]);
+                }
+
+                Center.AddTiles(toPut);
+                if (ArePlatesEmpty()) {
+                    Logger.WriteLine("Phase changed to filling");
+                    CurrentPlayer = 0;
+                    bool isSkiped = false;
+                    while (!Players[CurrentPlayer].hasFullBuffer()) {
+                        Logger.WriteLine($"Player {Players[CurrentPlayer].name} has no full buffer");
+                        if (Players[CurrentPlayer].ClearFloor()) {
+                            Logger.WriteLine($"Player {Players[CurrentPlayer].name} will start next turn");
+                            nextFirst = CurrentPlayer;
+                        }
+                        CurrentPlayer++;
+                        if (CurrentPlayer == Players.Length) {
+                            isSkiped = true;
+                            Logger.WriteLine("No player has full buffer");
+                            CurrentPlayer = nextFirst;
+                        }
+                    }
+
+                    if (!isSkiped) Phase = Phase.Placing;
+
+                }
+                else {
+                    CurrentPlayer++;
+                    if (CurrentPlayer == Players.Length) CurrentPlayer = 0;
+                }
+            }
+
+            #endregion
+        
+            return success;
+        }
+    
+        public bool Calculate(int col = Globals.EMPTY_CELL) {
+            Logger.WriteLine("Filling:");
+            Logger.WriteLine($"Player's data: {Players[CurrentPlayer]}");
+            if (Phase != Phase.Placing) {
+                Logger.WriteLine("Invalid Phase");
+                throw new IllegalOptionException("Invalid Phase");
+            }
+            int[] fullBuffers = Players[CurrentPlayer].FullBuffers();
+
+            if (col < 0 && isAdvanced) {
+                Logger.WriteLine("invalid col");
+                return false;
+            }
+            if (!isAdvanced) {
+                for (int tmp = 0; tmp < predefinedWall.GetLength(0); tmp++) {
+                    if (predefinedWall[fullBuffers[0], tmp] == Players[CurrentPlayer].GetBufferData(fullBuffers[0]).id) {
+                        col = tmp;
+                        break;
+                    }
+                }
+            }
+            bool isFilled = Players[CurrentPlayer].Fill(fullBuffers[0], col);
+            if (isFilled && fullBuffers.Length == 1) {
+                if (Players[CurrentPlayer].ClearFloor()) {
+                    Logger.WriteLine($"Player {Players[CurrentPlayer].name} will start next turn");
+                    nextFirst = CurrentPlayer;
+                }
+                CurrentPlayer++;
+                while (CurrentPlayer < Players.Length && !Players[CurrentPlayer].hasFullBuffer()) {
+                    Logger.WriteLine($"Player {Players[CurrentPlayer].name} has no full buffer");
+                    if (Players[CurrentPlayer].ClearFloor()) {
+                        Logger.WriteLine($"Player {Players[CurrentPlayer].name} will start next turn");
+                        nextFirst = CurrentPlayer;
+                    }
+                    CurrentPlayer++;
+                }
+
+                if (CurrentPlayer == Players.Length) {
+                    //All players finished phase 2
+                    if (isGameOver) {
+                        Logger.WriteLine("-------GGG-------A-------M------M--EEEEEE-------OOO----V-----V----EEEEEE----RRRR------");
+                        Logger.WriteLine("------G---G-----A-A------MM----MM--E-----------O---O---V-----V----E---------R---R-----");
+                        Logger.WriteLine("-----G---------A---A-----M-M--M-M--EEEE-------O-----O---V---V-----EEEE------RRRR------");
+                        Logger.WriteLine("-----G--GGG---AAAAAAA----M--MM--M--EEEE-------O-----O---V---V-----EEEE------R--R------");
+                        Logger.WriteLine("------G---G---A-----A----M------M--E-----------O---O-----V-V------E---------R---R-----");
+                        Logger.WriteLine("-------GGG---A-------A---M------M--EEEEEE-------OOO-------V-------EEEEEE----R---R-----");
+
+                        Phase = Phase.GameOver; //TODO: calculate bonuses, I guess
+                    }
+                    else {
+                        Logger.WriteLine("All players filled to the wall");
+                        CurrentPlayer = nextFirst;
+                        Phase = Phase.Taking;
+                        FillPlates();
+                    }
+                }
+            }
+        
+            return isFilled;
+        }
+    
+    
+        private bool ArePlatesEmpty() {
+            foreach (var plate in Plates) {
+                if (!plate.isEmpty) {
+                    return false;
+                }
+            }
+
+            return Center.isEmpty;
+        }
+
+        private void FillPlates() {
+            foreach (var plate in Plates) {
+                plate.SetTiles(storage.GetRandom(Globals.PLATE_VOLUME));
+            }
+
+            Center = new CenterPlate(Globals.TYPE_COUNT);
+            fisrtTaken = false;
+        }
+
+        private Player[] InitializePlayers(int playerCount, string[] playerNames) {
+            var players = new Player[playerCount];
+            for (int i = 0; i < playerCount; i++) {
+                players[i] = new Player(playerNames[i]);
+                players[i].OnWin += OnWin;
+            }
+            return players;
+        }
+
+        private void OnWin(object? sender, EventArgs args) {
+            if (Phase == Phase.Taking)
+                throw new Exception("Something went really wrong, win cant happen if we are not placing");
+            isGameOver = true;
+        }
+    }
+}
---
+++ AzulGUI/Assets/Resources/Utils.cs
@@ -0,0 +1,106 @@
+using System;
+using System.IO;
+
+namespace Azul {
+    public static class Globals {
+        public const int FIRST = 999;
+        public const int TYPE_COUNT = 5;
+        public const int WALL_DIMENSION = 5;
+        public const int TOTAL_TILE_COUNT = 100;
+        public const int PLATE_VOLUME = 4;
+        public const int EMPTY_CELL = -1;
+    }
+
+    public class IllegalOptionException : Exception {
+        public IllegalOptionException(string msg) : base(msg) { }
+    }
+
+    public static class Logger {
+        private static string fileName = "azul_log.txt";
+
+        public static void WriteLine(string msg) {
+            /* using StreamWriter log = new StreamWriter(Path.Combine(Environment.CurrentDirectory, fileName));
+             log.WriteLine(msg);
+             log.Flush();*/
+            File.AppendAllText(fileName, msg + Environment.NewLine);
+        }
+
+        public static void Write(string msg) {
+            /*using StreamWriter log = new StreamWriter(Path.Combine(Environment.CurrentDirectory, fileName));
+            log.Write(msg);
+            log.Flush();*/
+            File.AppendAllText(fileName, msg);
+        }
+    }
+
+    public enum Phase {
+        Taking = 1,
+        Placing = 2,
+        GameOver = 3
+    }
+
+    public struct Buffer {
+        public int size { get; private set; }
+        public int typeId { get; private set; }
+        public int filled { get; private set; }
+
+        public Buffer(int size) {
+            this.size = size;
+            typeId = Globals.EMPTY_CELL;
+            filled = Globals.EMPTY_CELL;
+        }
+
+        public bool Assign(int id, int count) {
+            //TODO:: maybe return int which is leak( what was after limit)
+            if (filled != Globals.EMPTY_CELL && typeId != id) {
+                Logger.WriteLine($"invalid type, needed {typeId}, got {id}");
+                return false; //TODO: break, return some kind of exeption
+            }
+
+            if (typeId == Globals.EMPTY_CELL) filled = 0;
+            typeId = id;
+            filled += count;
+            filled = Math.Min(filled, size);
+            Logger.WriteLine($"successfully filled, current state {filled} of {size}");
+            return true;
+        }
+
+        public bool Assign(Tile tile) {
+            return Assign(tile.id, tile.count);
+        }
+
+        public int FreeToFill() {
+            if (typeId == Globals.EMPTY_CELL) return size;
+            return size - filled;
+        }
+
+        public void Clear() {
+            typeId = Globals.EMPTY_CELL;
+            filled = Globals.EMPTY_CELL;
+        }
+
+        public bool IsFull() {
+            return size == filled;
+        }
+
+        public override string ToString() {
+            string outStr = "";
+            for (int i = 0; i < size; i++) {
+                if (i < filled) outStr += $"{typeId} ";
+                else outStr += $"{Globals.EMPTY_CELL} ";
+            }
+
+            return outStr;
+        }
+    }
+
+    public struct Tile {
+        public int id;
+        public int count;
+
+        public Tile(int id, int count) {
+            this.id = id;
+            this.count = count;
+        }
+    }
+}
