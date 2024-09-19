9338137f2206b270b4890a09b885a3af33e37671
JuhTyg010 <sadovsky11143@gmail.com>
2024-09-19 13:43:24 +0200

    implemented player's board

--- AzulGUI/Assets/Scripts/Board/PlayersBoard.cs
+++ AzulGUI/Assets/Scripts/Board/PlayersBoard.cs
@@ -8,6 +8,39 @@ namespace Board {
         
         [SerializeField] private GameObject wallHolder;
         [SerializeField] private List<GameObject> bufferHolders;
+        [SerializeField] private GameObject floorHolder;
+
+        private GameController gameController;
+
+        void Awake() {
+            gameController = FindObjectOfType<GameController>();
+        }
+
+        void Update(){
+            //TODO: get from gameManager playerData
+        }
+
+        public void Initialize(int id, GameController gameController) {
+            this.id = id;
+            this.gameController = gameController;
+            
+        }
+
+        private void UpdateWall(Azul.Player me) {
+            //TODO: wall should have some script which translates from int[,] to wall
+        }
+
+        private void UpdateBuffers(Azul.Player me) {
+            for (int i = 0; i < bufferHolders.Count; i++) {
+                var bufferLogic = bufferHolders[i].GetComponent<BufferHolder>();
+                var data = me.GetBufferData(i);
+                bufferLogic.LoadData(data.id, data.count);
+            }
+        }
+
+        private void UpdateFloor(Azul.Player me) {
+            //TODO: script for the floor and placing there
+        }
         
     }
 }
