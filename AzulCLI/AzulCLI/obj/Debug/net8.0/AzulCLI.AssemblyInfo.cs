ac8aafbdcf1d3b89d548be33486516cc2066063e
juhtyg <sadovsky11143@gmail.com>
2024-10-19 02:32:00 +0200

    moved Azul project to AzulCLI

--- AzulCLI/AzulCLI/obj/Debug/net8.0/AzulCLI.AssemblyInfo.cs
+++ AzulCLI/AzulCLI/obj/Debug/net8.0/AzulCLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzulCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057d1b5a2071f0292d3d5826129cda6193423409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f915aafd37e45d11879fedb1ea27e3204f8c7c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzulCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzulCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
