186f97b0cd7518dc3977f68f5823dd3afeb42c1d
juhtyg <sadovsky11143@gmail.com>
2025-01-05 13:26:55 +0100

    removed unused code

--- AzulCLI/randomBot/Bot.cs
+++ AzulCLI/randomBot/Bot.cs
@@ -26,49 +26,9 @@ namespace randomBot {
         }
 
         public string DoMove(Azul.Board board) {
-            /*Player me = board.Players[id];
-            List<int> possibleBuffers = PossibleBuffers(me);
-            List<int> possibleTypes = new List<int>();
-
-            bool isCorrectConfig = false;
-            int chosenBuffer = Globals.EMPTY_CELL;
-            int chosenType = Globals.EMPTY_CELL; //not chosen yet
-            int chosenPlate = Globals.EMPTY_CELL; //not chosen yet
-            int randomPos;
-
-            while (!isCorrectConfig) {
-                if (possibleBuffers.Count == 0) {
-                    chosenBuffer = Globals.WALL_DIMENSION; //floor
-                    possibleTypes = new List<int>();
-                    for (int i = 0; i < Globals.TYPE_COUNT; i++) possibleTypes.Add(i); //every type is possible 
-                }
-                else {
-                    randomPos = random.Next(0, possibleBuffers.Count);
-                    chosenBuffer = possibleBuffers[randomPos];
-                    possibleBuffers.RemoveAt(randomPos);
-                    possibleTypes = PossibleTypes(me, chosenBuffer);
-                }
-
-                while (true) {
-                    if (possibleTypes.Count == 0) break;
-                    randomPos = random.Next(0, possibleTypes.Count);
-                    chosenType = possibleTypes[randomPos];
-                    possibleTypes.RemoveAt(randomPos);
-
-                    List<int> possiblePlates = PossiblePlates(board, chosenType);
-                    if (possiblePlates.Count > 0) {
-                        isCorrectConfig = true;
-                        randomPos = random.Next(0, possiblePlates.Count);
-                        chosenPlate = possiblePlates[randomPos];
-                        break;
-                    }
-                }
-            }
-
-            return $"{chosenPlate} {chosenType} {chosenBuffer}";*/
+            
             var possibleMoves = PossibleMoves(board);
             int index = random.Next(possibleMoves.Count);
-            //TODO: select one with higher point gain, aka prefer to not put tiles to the floor
             Option option = new Option();
             int bestGain = Int32.MinValue;
             foreach (var possibleMove in possibleMoves) {
@@ -103,57 +63,6 @@ namespace randomBot {
             }
 
             return wall[row, column] == Globals.EMPTY_CELL;
-        }
-
-        private List<int> PossiblePlates(Azul.Board board, int chosenType) {
-            List<int> possiblePlates = new List<int>();
-
-            for (int i = 0; i < board.Plates.Length; i++) {
-                if (board.Plates[i].GetCounts()[chosenType].count > 0) {
-                    possiblePlates.Add(i);
-                }
-            }
-
-            if (board.Center.GetCounts()[chosenType].count > 0) possiblePlates.Add(board.Plates.Length);
-            return possiblePlates;
-        }
-
-        private List<int> PossibleTypes(Player me, int chosenBuffer) {
-            List<int> possibleTypes = new List<int>();
-            if (me.GetBufferData(chosenBuffer).id != Globals.EMPTY_CELL)
-                return new List<int>() { me.GetBufferData(chosenBuffer).id };
-            for (int i = 0; i < Globals.TYPE_COUNT; i++) {
-                bool isPossible = true;
-                for (int j = 0; j < Globals.WALL_DIMENSION; j++) {
-                    if (me.wall[chosenBuffer, j] == i) {
-                        isPossible = false;
-                        break;
-                    }
-                }
-
-                if (isPossible) {
-                    for (int j = 0; j < Globals.WALL_DIMENSION; j++) {
-                        if (PossibleCol(me.wall, chosenBuffer, j, i)) {
-                            possibleTypes.Add(i);
-                            break;
-                        }
-                    }
-                }
-            }
-
-            return possibleTypes;
-        }
-
-        private List<int> PossibleBuffers(Player me) {
-            List<int> posibleBuffers = new List<int>();
-            for (int i = 0; i < Globals.WALL_DIMENSION; i++) {
-                var data = me.GetBufferData(i);
-                if (data.count < i + 1) {
-                    posibleBuffers.Add(i);
-                }
-            }
-
-            return posibleBuffers;
         }
 
         private List<Option> PossibleMoves(Azul.Board board) {
